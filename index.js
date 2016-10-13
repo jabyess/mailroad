25b5f495070e6d3033feba3f344bc2fe5dac4a6b
James Byess <jbyess@morningconsult.com>
2016-10-13 18:31:15 -0400

    install bodyParser:

--- index.js
+++ index.js
@@ -10,16 +10,14 @@ dotenv.config();
 
 let app = express();
 
-
 app.engine('handlebars', hbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
 app.use('/components', express.static(path.join(__dirname, '/src/components')));
 app.use('/scripts', express.static(path.join(__dirname, 'dist')));
 app.use('/', routes);
 app.use('/api', API);
 
-db.sequelize.sync().then(()=>{
+db.sequelize.sync().then(() => {
 	app.listen(3000, function() {
 		console.log('Listening on port 3000!');
 	});
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,35 @@
 import express from 'express';
-import { db } from '../db/db.js';
+import db from '../models/index.js';
+import fs from 'fs';
+import util from 'util';
+import bodyParser from 'body-parser';
+
 let router = express.Router()
+let jsonParser = bodyParser.json();
 
-router.post('/createEmail', (req, res, next) => {
-	console.log(req);
-	db();
+router.post('/createEmail', jsonParser, (req, res, next) => {
+	console.log(req.body);
+	// let data = '';
+	// req.setEncoding('utf8');
+	// req.on('data', (chunk)=>{
+	// 	data += chunk;
+	// });
+	// console.log(res.body);
+	// req.on('end', ()=> {
+	// 	req.body = data;
+	// 	res.send(req.body);
+	// });
+	// console.log(req.query);
+	// console.log(req.params);
+	// console.log(req.route);
+	// console.log(req.route.stack);
 
-	res.body = 'posted to createEmail';
+	// db.email.upsert({
+	// 	email_content: req.body.email_content
+	// });
+
+	res.body = 'received yer post';
+	res.send(res.body);
 
 	next();
 })
