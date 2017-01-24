5860088f303f9784e994fb77e20603bca5331c46
James Byess <james.byess@gmail.com>
2017-01-24 17:02:51 -0500

    cleanup, refactor for babel config

--- server/lib/index.js
+++ server/lib/index.js
@@ -2,8 +2,7 @@ import dotenv from 'dotenv'
 import path from 'path'
 import express from 'express'
 import hbs from 'express-handlebars'
-import { API } from './server/routes/api.js'
-import db from './server/models/index.js'
+import { API } from '../routes/api.js'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 
 
@@ -29,17 +28,6 @@ app.get('*', (req, res) => {
 	})
 })
 
-
-// use this to force:true to make db drop all tables and reset schema
-// use when making schema changes
-// db.sequelize.sync({force: true}).then(() => {
-// 	app.listen(3000, function() {
-// 		console.log('Listening on port 3000!');
-// 	});
-// })
-
-db.sequelize.sync().then(() => {
-	app.listen(3000, function() {
-		console.log('Listening on port 3000!');
-	});
-})
+app.listen(3000, function() {
+	console.log('Listening on port 3000!');
+});
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv'
 import fs from 'fs'
 import path from 'path'
 import express from 'express'
-import db from '../models/index.js'
 import bodyParser from 'body-parser'
 import Utils from '../lib/utils.js'
 import multer from 'multer'
@@ -28,12 +27,12 @@ const s3Params = {
 let S3 = new AWS(s3Config)
 
 router.get('/listEmails', jsonParser, (req, res) => {
-	db.email.findAll({
-		order: '"updatedAt"DESC',
-		limit: 20
-	}).then((results) => {
-		res.send(results)
-	})
+	// db.email.findAll({
+	// 	order: '"updatedAt"DESC',
+	// 	limit: 20
+	// }).then((results) => {
+	// 	res.send(results)
+	// })
 })
 
 router.get('/templates', (req, res) => {
@@ -60,63 +59,33 @@ router.post('/compileTemplate', jsonParser, (req,res) => {
 
 router.get('/getEmail/:id', (req, res) => {
 	let id = req.params.id
-	db.email.findById(id).then((results) => {
-		//extract only the content we want from db
-		//sequelize returns a lot of extraneous data otherwise
-		//so we use .get and plain:true
-		let dataValues = results.get({ plain: true })
-		res.send(dataValues)
-	})
 })
 
 router.post('/createNewEmail', jsonParser, (req,res) => {
-	let { content, title } = req.body;
-	db.email.create({ content, title }).then((results) => {
-		let dataValues = results.get({ plain: true })
-		res.send(dataValues)
-	})
+	let { content, title } = req.body
+	// db.email.create({ content, title }).then((results) => {
+	// 	let dataValues = results.get({ plain: true })
+	// 	res.send(dataValues)
+	// })
 })
 
 router.post('/updateEmail', jsonParser, (req, res) => {
-	db.email.upsert(
-		Object.assign({}, req.body)
-	).then(() => {
-		res.sendStatus(200)
-	}).catch((err)=>{
-		console.log(err)
-		res.status(500).send(err)
-	})
+	// db.email.upsert(
+	// 	Object.assign({}, req.body)
+	// ).then(() => {
+	// 	res.sendStatus(200)
+	// }).catch((err)=>{
+	// 	console.log(err)
+	// 	res.status(500).send(err)
+	// })
 })
 
-router.post('/deleteEmail', jsonParser, (req, res) => {
-	let emailsToDelete = req.body.selectedEmails
-	db.email.destroy({
-		where: { id: emailsToDelete }
-	})
-	.then((instance) => {
-		//returns number of affected rows
-		res.send('deleted', instance, 'emails')
-	})
-})
+// router.post('/deleteEmail', jsonParser, (req, res) => {
+// })
 
-router.post('/copyEmail', jsonParser, (req, res) => {
-	const id = req.body.id[0]
-	db.email.findById(id)
-		.then((instance) => {
-			console.log(instance.get({plain:true}))
-			return instance.get({plain: true})
-		})
-		.then((newEmail)=> {
-			db.email.create({
-				content: newEmail.content,
-				template: newEmail.template,
-				title: newEmail.title,
-			})
-			.then(()=> {
-				res.sendStatus(200)
-			})
-		})
-})
+// router.post('/copyEmail', jsonParser, (req, res) => {
+// 	const id = req.body.id[0]
+// })
 
 // AWS_BUCKET + .s3.amazonaws.com/ + data.key
 router.get('/s3/list', (req, res) => {
